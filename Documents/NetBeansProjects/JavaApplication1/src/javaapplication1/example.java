8edbe4220d24cb465a338548157ec00d9c7f7005
I AM XU ZIXUAN <xu.zix@husky.neu.edu>
2017-01-09 20:18:59 -0500

    new

---
+++ Documents/NetBeansProjects/JavaApplication1/src/javaapplication1/example.java
@@ -0,0 +1,15 @@
+
+package javaapplication1;
+
+/**
+ *
+ * @author Accelarator
+ */
+public class example {
+
+    
+    public static void main(String[] args) {
+        System.out.println("hello World!");
+    }
+    
+}
